2ff81b0b3c0e51d90461d59429038a9b1aa9e856
Andrew Park <blooominho@gmail.com>
2025-08-25 14:35:50 -0400

    Added Steps to follow to template code

--- longest_common.c
+++ longest_common.c
@@ -9,9 +9,33 @@ Arguments:
     second_line: temporary space to store what is read from the second line of the file
     size: size of line
 Return:
-    length of the longest overlapping substring between two strings
+    length of the longest common substring between two strings
 */
 int longest_common(char* filename, char* first_line, char* second_line, int size)
 {
-    // EDIT HERE!
+    // EDIT THIS FUNCTION
+    // You may follow the steps and use the codes given, but you may also ignore and make your own moves as well.
+
+    // Step 1: Open the file
+    //         Don't forget to handle failures
+
+
+    // Step 2: Read two lines from the file
+    //         Save the first line at first_line and second line at second_line
+    //         Don't forget to handle the failures
+
+
+    // Step 3: Remove each line's newline character
+    first_line[strcspn(first_line, "\r\n")]  = '\0';
+    second_line[strcspn(second_line, "\r\n")] = '\0';
+
+    // Step 4: Find the longest common substring between the two strings
+    //         Probably, you would have to loops (that are nested)
+    int max_length = 0;         // Maximum length of common substring that was found
+
+
+
+
+    // Step 5 :Return the maximum length of common substrings
+    return max_length;
 }
