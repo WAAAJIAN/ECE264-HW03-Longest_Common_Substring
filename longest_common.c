eab2f14fff1a700a40815ab9253a12ced82f1c47
Wei Jian Lim <lim540@ececomp3.ecn.purdue.edu>
2025-09-10 18:35:49 -0400

    done

--- longest_common.c
+++ longest_common.c
@@ -18,23 +18,42 @@ int longest_common(char* filename, char* first_line, char* second_line, int size
 
     // Step 1: Open the file
     //         Don't forget to handle failures
-
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        return -1;
+    }
 
     // Step 2: Read two lines from the file
     //         Save the first line at first_line and second line at second_line
     //         Don't forget to handle the failures
 
+    if (fgets(first_line, size, file) == NULL || fgets(second_line, size, file) == NULL) {
+        fclose(file);
+        return -1;
+    }
 
     // Step 3: Remove each line's newline character
     first_line[strcspn(first_line, "\r\n")]  = '\0';
     second_line[strcspn(second_line, "\r\n")] = '\0';
 
     // Step 4: Find the longest common substring between the two strings
-    //         Probably, you would have to loops (that are nested)
+    //         Probably, you would have to loop (that are nested)
     int max_length = 0;         // Maximum length of common substring that was found
-
-
-
+    for (int i = 0; first_line[i] != '\0'; i++) {
+        for (int j = 0; second_line[j] != '\0'; j++) {
+            int current_length = 0; // Length of the current common substring
+            while (first_line[i + current_length] == second_line[j + current_length] && second_line[j+current_length] != '\0') {
+                // printf("%d\n", current_length);
+                current_length++;
+                if (current_length > max_length) {
+                    max_length = current_length;
+                }
+            }
+            current_length = 0;
+        }
+    }
+    
+    fclose(file);
 
     // Step 5 :Return the maximum length of common substrings
     return max_length;
